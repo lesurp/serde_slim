29d809a58494f4d9286112074392f77f50e3c7d3
Paul Lesur <gitconfig@lesurpaul.fr>
2019-07-26 16:57:59 +0200

    fix: proper tree item (ItemStruct rather tham ExprStruct)

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ use quote::quote;
 use std::collections::HashMap;
 use syn::Ident;
 
-struct ExprStructs {
+struct ItemStructs {
     ident_mapping: HashMap<Ident, Ident>,
-    structs: Vec<syn::ExprStruct>,
+    structs: Vec<syn::ItemStruct>,
 }
 
-impl syn::parse::Parse for ExprStructs {
+impl syn::parse::Parse for ItemStructs {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let mut ident_mapping = HashMap::new();
         let mut structs = Vec::new();
@@ -17,17 +17,9 @@ impl syn::parse::Parse for ExprStructs {
         let _ = input.parse::<syn::Token![,]>()?;
 
         while !input.is_empty() {
-            let _ = input.parse::<syn::Token![struct]>();
-            let next_struct = input.parse::<syn::ExprStruct>()?;
+            let next_struct = input.parse::<syn::ItemStruct>()?;
             // TODO<24-07-19, Paul Lesur> unwrap!
-            let orig_name = next_struct
-                .path
-                .segments
-                .last()
-                .unwrap()
-                .value()
-                .ident
-                .clone();
+            let orig_name = next_struct.ident.clone();
             let slim_name = Ident::new(
                 &(ident_prefix.clone() + &orig_name.to_string()),
                 proc_macro2::Span::call_site(),
@@ -36,27 +28,27 @@ impl syn::parse::Parse for ExprStructs {
             structs.push(next_struct);
             input.is_empty();
         }
-        Ok(ExprStructs {
+        Ok(ItemStructs {
             ident_mapping,
             structs,
         })
     }
 }
 
-impl ExprStructs {
-    fn orig_structs(&self) -> &Vec<syn::ExprStruct> {
+impl ItemStructs {
+    fn orig_structs(&self) -> &Vec<syn::ItemStruct> {
         &self.structs
     }
 
-    fn rename_struct_type(&self, expr_struct: &mut syn::ExprStruct) {
+    fn rename_struct_type(&self, expr_struct: &mut syn::ItemStruct) {
         // TODO<24-07-19, Paul Lesur> unwrap!
-        let orig_name = &expr_struct.path.segments.last().unwrap().value().ident;
+        let orig_name = &expr_struct.ident;
         // TODO<24-07-19, Paul Lesur> unwrap!
         let slim_name = self.ident_mapping.get(&orig_name).unwrap();
-        ExprStructs::set_type(&mut expr_struct.path, slim_name.clone());
+        expr_struct.ident = slim_name.clone();
     }
 
-    fn should_keep_field(f: &syn::FieldValue) -> bool {
+    fn should_keep_field(f: &syn::Field) -> bool {
         for attr in f.attrs.iter() {
             if let syn::AttrStyle::Inner(_) = attr.style {
                 continue;
@@ -92,12 +84,18 @@ impl ExprStructs {
         true
     }
 
-    fn update_field_type(&self, mut field_value: syn::FieldValue) -> syn::FieldValue {
-        match &mut field_value.expr {
-            syn::Expr::Path(expr_path) => {
-                let orig_name = &expr_path.path.segments.last().unwrap().value().ident;
+    fn update_field_type(&self, mut field: syn::Field) -> syn::Field {
+        match &mut field.ty {
+            syn::Type::Path(type_path) => {
+                let orig_name = &type_path.path.segments.last().unwrap().value().ident;
                 if let Some(slim_name) = self.ident_mapping.get(&orig_name) {
-                    ExprStructs::set_type(&mut expr_path.path, slim_name.clone());
+                    type_path
+                        .path
+                        .segments
+                        .last_mut()
+                        .unwrap()
+                        .value_mut()
+                        .ident = slim_name.clone();
                 }
             }
             _ => panic!(
@@ -105,33 +103,42 @@ impl ExprStructs {
                             but they do appear as 'syn::Expr::Path' when parsed through syn"
             ),
         }
-        field_value
+        field
     }
 
-    fn updated_field(&self, field_value: syn::FieldValue) -> Option<syn::FieldValue> {
-        if ExprStructs::should_keep_field(&field_value) {
-            Some(self.update_field_type(field_value))
+    fn updated_field(&self, field: syn::Field) -> Option<syn::Field> {
+        if ItemStructs::should_keep_field(&field) {
+            Some(self.update_field_type(field))
         } else {
             None
         }
     }
 
-    fn set_type(p: &mut syn::Path, i: Ident) {
-        // TODO<24-07-19, Paul Lesur> unwrap!
-        p.segments.last_mut().unwrap().value_mut().ident = i;
-    }
-
-    fn slim_structs(&self) -> Vec<syn::ExprStruct> {
+    fn slim_structs(&self) -> Vec<syn::ItemStruct> {
         let mut v = Vec::new();
 
         for orig_struct in &self.structs {
             let mut slim_struct = orig_struct.clone();
             self.rename_struct_type(&mut slim_struct);
-            slim_struct.fields = slim_struct
-                .fields
-                .into_iter()
-                .filter_map(|f| self.updated_field(f))
-                .collect();
+            slim_struct.fields = match slim_struct.fields {
+                syn::Fields::Named(mut named) => {
+                    named.named = named
+                        .named
+                        .into_iter()
+                        .filter_map(|f| self.updated_field(f))
+                        .collect();
+                    named.into()
+                }
+                syn::Fields::Unnamed(mut unnamed) => {
+                    unnamed.unnamed = unnamed
+                        .unnamed
+                        .into_iter()
+                        .filter_map(|f| self.updated_field(f))
+                        .collect();
+                    unnamed.into()
+                }
+                syn::Fields::Unit => slim_struct.fields,
+            };
 
             v.push(slim_struct);
         }
@@ -142,17 +149,17 @@ impl ExprStructs {
 
 #[proc_macro]
 pub fn serde_slim(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as ExprStructs);
+    let input = syn::parse_macro_input!(input as ItemStructs);
 
     let mut output = quote! {};
     for orig_struct in input.orig_structs() {
-        output = quote! { struct #orig_struct
+        output = quote! { #orig_struct
             #output
         };
     }
 
     for slim_struct in input.slim_structs() {
-        output = quote! { struct #slim_struct
+        output = quote! { #slim_struct
             #output
         };
     }
