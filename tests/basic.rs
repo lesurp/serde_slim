8af54ca55df078a26f8df1e5b3991dad4478438e
Paul Lesur <gitconfig@lesurpaul.fr>
2019-07-26 16:46:19 +0200

    impr: more thorough test regarding access (pub/priv)

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,25 +1,32 @@
 #![feature(custom_attribute)]
 
-use serde_slim::serde_slim;
+mod baz {
+    use serde_slim::serde_slim;
+    serde_slim! {
+    Serialized,
+    pub struct Foo {
+        pub x: i32,
+        #[serde(skip_serializing)]
+        y: i32,
+    }
 
-serde_slim! {
-Serialized,
-struct Foo {
-    x: i32,
-    #[serde(skip_serializing)]
-    y: i32,
-}
+    pub struct Bar {
+        pub foo: Foo
+    }
+    }
 
-struct Bar {
-    foo: Foo
-}
+    impl Foo {
+        pub fn new(x: i32, y: i32) -> Self {
+            Foo { x, y }
+        }
+    }
 }
 
 // compiling is the test
 fn main() {
-    let f = Foo { x: 0, y: 1 };
-    let sf = SerializedFoo { x: 0, };
+    let f = baz::Foo::new(0, 1);
+    let sf = baz::SerializedFoo { x: 0 };
 
-    let b = Bar { foo: f };
-    let sb = SerializedBar { foo: sf };
+    let b = baz::Bar { foo: f };
+    let sb = baz::SerializedBar { foo: sf };
 }
